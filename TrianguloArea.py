3324e1aae2c8c8bd2619ad16c419f2b2164eda8b
elenuco <elenuco36@gmail.com>
2020-12-02 05:23:34 +0000

    arreglos

--- TrianguloArea.py
+++ TrianguloArea.py
@@ -1,1 +1,5 @@
 #calcular el area de un triangulo
+base= int(input('Ingrese la base del triangulo: '))
+altura= int (input('Ingrese altura'))
+area= base*altura/2
+print('El area del triangulo es '+ str(area))
